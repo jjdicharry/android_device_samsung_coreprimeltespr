6772b840baedb9cc7b01db2a7edaa9e8e23086a5
Jeremiah Dicharry <jjdicharry@gmail.com>
2017-03-12 17:16:56 -0700

    Made sure no recovery updater was added yet.

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -14,11 +14,11 @@ include $(CLEAR_VARS)
 
 # Edify extension functions for doing bootloader updates on manta devices.
 
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES += bootable/recovery
-LOCAL_SRC_FILES := recovery_updater.c
+# LOCAL_MODULE_TAGS := optional
+# LOCAL_C_INCLUDES += bootable/recovery
+# LOCAL_SRC_FILES := recovery_updater.c
 
 # should match TARGET_RECOVERY_UPDATER_LIBS set in BoardConfig.mk
-LOCAL_MODULE := librecovery_updater_coreprimeltespr
+# LOCAL_MODULE := librecovery_updater_coreprimeltespr
 
 include $(BUILD_STATIC_LIBRARY)
